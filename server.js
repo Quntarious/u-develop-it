5937b2253e30e0e5544da92301b51824e46e4fb2
Quntarious <90361495+Quntarious@users.noreply.github.com>
2022-03-23 15:38:20 -0500

    Database calls

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'Quntarious Brown',
+      // Your MySQL password
+      password: 'Stankqqb01!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
