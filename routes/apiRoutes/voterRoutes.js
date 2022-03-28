99fede7fed5b20c6f518b7c7ac4e9bcf05359ee7
Quntarious <90361495+Quntarious@users.noreply.github.com>
2022-03-28 14:47:00 -0500

    vote totals

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -37,16 +37,16 @@ router.get('/voter/:id', (req, res) => {
 });
 
 // Create a voter
-router.post('/voter', ({ body }, res) => {
+router.post('/vote', ({ body }, res) => {
   // Data validation
-  const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+  const errors = inputCheck(body, 'voter_id', 'candidate_id');
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
 
-  const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
-  const params = [body.first_name, body.last_name, body.email];
+  const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+  const params = [body.voter_id, body.candidate_id];
 
   db.query(sql, params, (err, result) => {
     if (err) {
@@ -55,7 +55,8 @@ router.post('/voter', ({ body }, res) => {
     }
     res.json({
       message: 'success',
-      data: body
+      data: body,
+      changes: result.affectedRows
     });
   });
 });
